6fce233a1901f6a5d7c604171a244b28bcd7881d
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2023-06-23 23:43:55 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,3 +13,21 @@ telegram_bot_sendtext("Connection successful")
 time.sleep(5)
 i=6369877.269
 telegram_bot_sendtext(i)
+start_time=time.time()
+while True:
+    bot_seconds = 27
+        if 25<bot_seconds<28:
+            elapsed_time = time.time() - start_time
+            if elapsed_time<60:
+              status = f"UP Running...  {elapsed_time:.2f}s"
+              telegram_bot_sendtext(status)
+            elif 60 < elapsed_time < 3600:
+              min = elapsed_time // 60
+              status = f"UP Running...  {min:.2f} min"
+              telegram_bot_sendtext(status)
+            elif 3600 < elapsed_time < 86400 :
+              hours = elapsed_time // 3600
+              mins = (elapsed_time-hours*3600)//60
+              status = f"UP Running...  {hours:.2f} hr {mins:.2f} min"
+              telegram_bot_sendtext(status)
+    time.sleep(2)
