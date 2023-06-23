35da1c152dea64cb339d98e800ea034af66e8844
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2023-06-23 18:32:38 +0000

    Update upcode_script.py

--- upcode_script.py
+++ upcode_script.py
@@ -1,5 +1,3 @@
-import telegram
-from telegram.ext import Updater
 from iqoptionapi.stable_api import IQ_Option
 import logging      
 import pandas as pd
@@ -8,6 +6,23 @@ import iqoptionapi
 import tradingview_ta
 import time
 import sys
+import requests
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = '6283203048:AAGgOl-o6Itm3D1mw4_Omcf-g4t260vixN8'
+    bot_chatID = '1155462778'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + \
+                '&parse_mode=MarkdownV2&text=' + str(bot_message)  # Convert i to a string
+    response = requests.get(send_text)
+    return response.json()
+
+i = 1
+while True:
+    telegram_bot_sendtext(i)
+    i += 1
+    time.sleep(1)
+
+
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 I_want_money=IQ_Option("arishisgay@gay.com","arishisgay@gay.com")
@@ -23,12 +38,6 @@ I_want_money.get_server_timestamp()
 I_want_money.change_balance(MODE)
                         #MODE: "PRACTICE"/"REAL
 I_want_money.get_balance()
-telegram_token = "6283203048:AAGgOl-o6Itm3D1mw4_Omcf-g4t260vixN8"
-bot = telegram.Bot(token=telegram_token)
-def send_status_message(message):
-    chat_id = "1155462778"
-    bot.send_message(chat_id=chat_id, text=message)
-
 API = IQ_Option("arishisgay@gay.com", "arishisgay@gay.com")
 check, reason = API.connect()
 if not check:
@@ -58,17 +67,26 @@ handler = TA_Handler(
 )
 send_status_message(balance_before)
 start_time = time.time()
-i = 1
+def telegram_bot_sendtext(bot_message):
+    bot_token = '6283203048:AAGgOl-o6Itm3D1mw4_Omcf-g4t260vixN8'
+    bot_chatID = '1155462778'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + \
+                '&parse_mode=MarkdownV2&text=' + str(bot_message)  # Convert i to a string
+    response = requests.get(send_text)
+    return response.json()
+def get_remaining_seconds(x):
+    current_time = time.localtime()
+    current_minute = current_time.tm_min
+    remaining_seconds = (x - (current_minute % x)) * 60 - current_time.tm_sec
+    return remaining_seconds
 while True:
     current_price = API.get_candles("EURUSD", 60 * x, 1, time.time())[0]["close"]
     analysis = handler.get_analysis()
     ema = analysis.indicators["EMA100"]
-    #Uptrend
-    # if (current_price > ema) and profit_result<30 and loss_result<1 :
     if (current_price > ema) :
-        elapsed_time = time.time() - start_time
-        status = f"UP-Running... - {elapsed_time:.2f}s"
-        print(status, end="\r")
+        bot_seconds = get_remaining_seconds(15)
+        if bot_seconds :
+            telegram_bot_sendtext()
         candles = API.get_candles("EURUSD", 60 * x, 100, time.time())
         close_prices = [candle["close"] for candle in candles]
         df = pd.DataFrame(candles)        
@@ -81,11 +99,6 @@ while True:
             if not trade_placed:
                 direction = "call"
                 now = time.time()
-                def get_remaining_seconds(x):
-                  current_time = time.localtime()
-                  current_minute = current_time.tm_min
-                  remaining_seconds = (x - (current_minute % x)) * 60 - current_time.tm_sec
-                  return remaining_seconds
                 remaining_seconds = get_remaining_seconds(x)
 
                 if remaining_seconds >=31 and remaining_seconds <= 90:
@@ -107,11 +120,11 @@ while True:
                     trade_placed = True
                 else:
                     print("Error placing trade:")
-    #downtrend
+
     elif (current_price <= ema) :
-        elapsed_time = time.time() - start_time
-        status = f"Down-Running... - {elapsed_time:.2f}s"
-        print(status, end="\r")
+        bot_seconds = get_remaining_seconds(15)
+        if bot_seconds :
+            telegram_bot_sendtext()
         candles = API.get_candles("EURUSD", 60 * x, 100, time.time())
         close_prices = [candle["close"] for candle in candles]
         df = pd.DataFrame(candles)        
@@ -123,13 +136,7 @@ while True:
             if not trade_placed:
                 direction = "put"
                 now = time.time()
-                def get_remaining_seconds(x):
-                  current_time = time.localtime()
-                  current_minute = current_time.tm_min
-                  remaining_seconds = (x - (current_minute % x)) * 60 - current_time.tm_sec
-                  return remaining_seconds
                 remaining_seconds = get_remaining_seconds(x)
-
                 if remaining_seconds >=31 and remaining_seconds <= 90:
                     value = 1
                 elif remaining_seconds >= 91 and remaining_seconds <= 150:
