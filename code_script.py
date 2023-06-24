b3912eecb51738afb56850174b78bdb4140e5808
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2023-06-24 01:36:50 +0000

    Update code_script.py

--- code_script.py
+++ code_script.py
@@ -6,6 +6,15 @@ import iqoptionapi
 import tradingview_ta
 import time
 import sys
+import requests
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = '6283203048:AAGgOl-o6Itm3D1mw4_Omcf-g4t260vixN8'
+    bot_chatID = '1155462778'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + \
+                '&parse_mode=MarkdownV2&text=' + str(bot_message).replace('.', '\\.')  # Escape the dot character
+    response = requests.get(send_text)
+    return response.json()
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 I_want_money=IQ_Option("dharshan.cs05@gmail.com","It'smecsd050402")
